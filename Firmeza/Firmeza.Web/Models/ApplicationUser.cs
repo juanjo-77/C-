6432e441b41350e39cb2438761a039ad8623701b
Juan Jose Peña <penamunozjuanjose64@gmail.com>
2026-05-21 15:27:19 -0500

    Delete Firmeza directory

--- Firmeza/Firmeza.Web/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Firmeza.Web.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public string NombreCompleto { get; set; } = string.Empty;
-    }
-}
